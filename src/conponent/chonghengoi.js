99f9d1d7ee4bb22b227109df555a5f35c9724eb4
Nguyen Duc Huu <nguyenduchuu2k2@gmail.com>
2023-11-15 22:50:09 +0700

    fix chỗ ngồi

--- src/conponent/chonghengoi.js
+++ src/conponent/chonghengoi.js
@@ -27,51 +27,53 @@ function ChonGheNgoi() {
     };
     return (
         <>
-            <div className="container">
+            <div className="ms-3 positon_sticky col-lg-6">
                 <div className="mt-4">
                     <p className="tilet_luotdi">Lượt đi</p>
                 </div>
-                <div className="d-flex">
-                    <div className="col-lg-6 pe-3">
-                        <table className="col-lg-12">
-                            <thead className="col-lg-12 ">
-                                <tr className="col-lg-12 d-flex justify-content-between header_table">
-                                    <th className="col-lg-1 d-flex"></th>
-                                    <th className="col-lg-1 d-flex justify-content-center">STT</th>
-                                    <th className="col-lg-5 d-flex justify-content-center">Họ tên</th>
-                                    <th className="col-lg-2 d-flex justify-content-center">Ghế đi</th>
-                                    <th className="col-lg-3 d-flex justify-content-center">Loại vé</th>
-                                </tr>
-                            </thead>
-                            {DetailListHK &&
-                                DetailListHK.map((value, index) => (
-                                    <tbody key={value + index}>
-                                        <tr className="col-lg-12 d-flex justify-content-between align-items-center value_table">
-                                            <td className="col-lg-1">
-                                                <input
-                                                    type="radio"
-                                                    name="checkbtn"
-                                                    id={`hanhkhach_id${index}`}
-                                                    onChange={() => handleRadioChange(index)}
-                                                />
-                                            </td>
-                                            <td className="col-lg-1 d-flex justify-content-center">
-                                                <label htmlFor={`hanhkhach_id${index}`}>{index + 1}</label>
-                                            </td>
-                                            <td className="col-lg-5 d-flex justify-content-center">
-                                                <label htmlFor={`hanhkhach_id${index}`}>{value.HoTenHK}</label>
-                                            </td>
-                                            <td className="col-lg-2 d-flex justify-content-center">
-                                                <label htmlFor={`hanhkhach_id${index}`}>{value.TenGhe}</label>
-                                            </td>
-                                            <td className="col-lg-3 d-flex justify-content-center">
-                                                <label htmlFor={`hanhkhach_id${index}`}>Người lớn</label>
-                                            </td>
-                                        </tr>
-                                    </tbody>
-                                ))}
-                        </table>
-                    </div>
+                <div className=" pe-3 ">
+                    <table className="col-lg-12">
+                        <thead className="col-lg-12 ">
+                            <tr className="col-lg-12 d-flex justify-content-between header_table">
+                                <th className="col-lg-1 d-flex"></th>
+                                <th className="col-lg-1 d-flex justify-content-center">STT</th>
+                                <th className="col-lg-5 d-flex justify-content-center">Họ tên</th>
+                                <th className="col-lg-2 d-flex justify-content-center">Ghế đi</th>
+                                <th className="col-lg-3 d-flex justify-content-center">Loại vé</th>
+                            </tr>
+                        </thead>
+                        {DetailListHK &&
+                            DetailListHK.map((value, index) => (
+                                <tbody key={value + index}>
+                                    <tr className="col-lg-12 d-flex justify-content-between align-items-center value_table">
+                                        <td className="col-lg-1">
+                                            <input
+                                                type="radio"
+                                                name="checkbtn"
+                                                id={`hanhkhach_id${index}`}
+                                                onChange={() => handleRadioChange(index)}
+                                            />
+                                        </td>
+                                        <td className="col-lg-1 d-flex justify-content-center">
+                                            <label htmlFor={`hanhkhach_id${index}`}>{index + 1}</label>
+                                        </td>
+                                        <td className="col-lg-5 d-flex justify-content-center">
+                                            <label htmlFor={`hanhkhach_id${index}`}>{value.HoTenHK}</label>
+                                        </td>
+                                        <td className="col-lg-2 d-flex justify-content-center">
+                                            <label htmlFor={`hanhkhach_id${index}`}>{value.TenGhe}</label>
+                                        </td>
+                                        <td className="col-lg-3 d-flex justify-content-center">
+                                            <label htmlFor={`hanhkhach_id${index}`}>Người lớn</label>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            ))}
+                    </table>
+                </div>
+            </div>
+            <div className="container" style={{ position: 'relative', top: '-180px' }}>
+                <div className="d-flex justify-content-end">
                     <div className="col-lg-6">
                         <div className="col-lg-12">
                             <h4>Vị trí chỗ ngồi</h4>
@@ -80,24 +82,47 @@ function ChonGheNgoi() {
                         <div className="col-lg-12 d-flex flex-wrap">
                             {allGheNgoi &&
                                 allGheNgoi.map((value, index) => (
-                                    <div className="mb-3 col-lg-2" key={value + index}>
-                                        <button
-                                            className="btn_vitri "
-                                            style={{
-                                                backgroundColor:
-                                                    (value.MaKhachDiChung && '#EE4E4E') ||
-                                                    (checkChoose(value.MaGhe) ? '#092c7a' : ''),
-                                                color:
-                                                    (value.MaKhachDiChung && '#fff') ||
-                                                    (checkChoose(value.MaGhe) ? '#fff' : ''),
-                                            }}
-                                            onClick={() => {
-                                                handleGheChange(value.MaGhe, value.TenGhe);
-                                            }}
-                                        >
-                                            {value.TenGhe}
-                                        </button>
-                                    </div>
+                                    <>
+                                        {value.MaGhe <= 6 ? (
+                                            <div className="mb-3 col-lg-4" key={value + index}>
+                                                <button
+                                                    className="btn_vitri "
+                                                    style={{
+                                                        backgroundColor:
+                                                            (value.MaKhachDiChung && '#EE4E4E') ||
+                                                            (checkChoose(value.MaGhe) ? '#092c7a' : ''),
+                                                        color:
+                                                            (value.MaKhachDiChung && '#fff') ||
+                                                            (checkChoose(value.MaGhe) ? '#fff' : ''),
+                                                    }}
+                                                    onClick={() => {
+                                                        handleGheChange(value.MaGhe, value.TenGhe);
+                                                    }}
+                                                >
+                                                    {value.TenGhe}
+                                                </button>
+                                            </div>
+                                        ) : (
+                                            <div className="mb-3 col-lg-3" key={value + index}>
+                                                <button
+                                                    className="btn_vitri "
+                                                    style={{
+                                                        backgroundColor:
+                                                            (value.MaKhachDiChung && '#EE4E4E') ||
+                                                            (checkChoose(value.MaGhe) ? '#092c7a' : ''),
+                                                        color:
+                                                            (value.MaKhachDiChung && '#fff') ||
+                                                            (checkChoose(value.MaGhe) ? '#fff' : ''),
+                                                    }}
+                                                    onClick={() => {
+                                                        handleGheChange(value.MaGhe, value.TenGhe);
+                                                    }}
+                                                >
+                                                    {value.TenGhe}
+                                                </button>
+                                            </div>
+                                        )}
+                                    </>
                                 ))}
                         </div>
                     </div>
